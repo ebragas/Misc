fe553a4f03751403db45266f35ab973d9aa2104c
Eric Bragas <ebragas@designmind.com>
2016-10-25 22:59:46 -0700

    Refined code, added cipher function for modularity, cleaned up formatting/comments, add input value validity checks

--- C/CS50/pset2/caesar.c
+++ C/CS50/pset2/caesar.c
@@ -7,32 +7,51 @@
 #include <string.h>
 #include <ctype.h>
 
+void cipher(char c, int key);
+
 int main(int argc, char* argv[]) {
 
-    // Check for valid command-line arguments
-    if (argc <> 2) {
+    // Check for valid arguments
+    if (argc != 2) {
         printf("Usage: ./caesar [key]\n");
         return 1;
     }
 
     int k = atoi(argv[1]) % 26;
+
+    // Check key is non-negative
+    if (k < 0) {
+      printf("Key must be non-negative\n");
+    }
+
+    // Get string; Check not null
     char* p = GetString();
 
-    for (int i = 0, n = strlen(p); i < n; i++) {
+    if (p != NULL) {
 
-        // Cipher Alpha Only
-        if ( isalpha(p[i]) ) {
+      // Loop through string; Encrypt if alpha (preserve case); Print standard out
+      for (int i = 0, n = strlen(p); i < n; i++) {
 
-            if ( (isupper(p[i]) && p[i] + k > 90) | (islower(p[i]) && p[i] + k > 122) ) {
-                printf("%c", p[i] + k - 26);
-            } else {
-                printf("%c", p[i] + k);
-            }
+          // Cipher Alpha Only
+          if ( isalpha(p[i]) ) {
 
-        } else {
-            printf("%c", p[i]);   // Non-alpha
-        }
+              if ( (isupper(p[i]) && p[i] + k > 90) | (islower(p[i]) && p[i] + k > 122) ) {
+                  cipher(p[i], k - 26);
+              } else {
+                  cipher(p[i], k);
+              }
+
+          } else {
+              cipher(p[i], 0);   // Non-alpha
+          }
+      }
     }
+
     printf("\n");   // End line
     return 0;
+}
+
+// Function: Print character offset by key
+void cipher(char c, int key) {
+  printf("%c", c + key);
 }

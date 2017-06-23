0d1159de6fed98e9ec818df90e819673e9719056
Eric Bragas <ebragas@designmind.com>
2017-06-22 20:19:04 -0700

    Developing page and profile iterator and extractor

---
+++ python/web scraping/beautifulsoup_tutorial.py
@@ -0,0 +1,63 @@
+
+# pylint: disable=C0103
+
+import csv
+import requests as r
+from bs4 import BeautifulSoup as bs
+
+url = 'https://www.concretedisciples.com'
+
+with open(r'c:\data\park_urls.csv', 'w', newline='') as csvfile:
+    fieldnames = ['park_name', 'park_url']
+    writer = csv.DictWriter(csvfile, fieldnames, quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+
+    for i in range(1, 3):
+        # request page; make soup; close page
+        print(url + "&page=" + str(i))
+        page = r.get(url, params={'limit': 50, 'page': i})
+        soup = bs(page.content, 'html.parser')
+        page.close()
+
+        # create list or urls
+        park_dict = {}
+        for e in soup.find_all('div', {'class': 'jrContentTitle'}):
+            # park_name = e.a.string
+            park_url = e.a['href']
+
+            # make profile soup
+            profile_page = r.get(url + park_url)
+            profile_soup = bs(profile_page.content, 'html.parser')
+            profile_page.close()
+
+            # skateparkname
+            park_name_tag = profile_soup.find('div', {'class': 'jrFieldRow jrSkateparkname'})
+            if park_name_tag is not None:
+                park_name = park_name_tag.contents[-1].string
+
+                # rip
+                # freepay
+                # indoors
+                # surface
+                # proshop
+
+            # extract construction
+            
+            # extract location
+
+            # extract contacts
+
+            # write data
+            writer.writerow({'park_name': park_name, 'park_url': park_url})
+
+
+# with open(r'c:\data\park_urls.csv', 'w') as csvfile:
+#     fieldnames = ['park_url']
+#     # writer = csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+#     writer = csv.DictWriter(csvfile, fieldnames)
+
+    # writer.writerow(fieldnames)
+    # writer.writeheader()
+
+    # for row in park_dict:
+    #     writer.writerow(row)

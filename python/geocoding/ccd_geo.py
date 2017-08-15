d9331983625a7eec9af92a3d0fdcccc19ae99d52
Eric Bragas <ebragas@designmind.com>
2017-08-15 09:58:10 -0700

    Init geocoding script

---
+++ python/geocoding/ccd_geo.py
@@ -0,0 +1,12 @@
+
+import csv
+import googlemaps
+
+SOURCE_FILE = r'c:\data\skateparks\parks.csv'
+KEY = 'AIzaSyCq-NNfZ6knfLR56b2XlTiILMMOyt2y534'
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

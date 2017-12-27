09745cf61f38d56f6041f605b02dc6fb3e5f731e
Eric Bragas <eric@designmind.com>
2017-12-27 15:14:24 -0800

    Start evernote utilities

---
+++ Python/evernote/daily_note.py
@@ -0,0 +1,14 @@
+
+from evernote.api.client import EvernoteClient
+import evernote.edam.type.ttypes as Types
+
+oauth_token = "S=s1:U=94432:E=167d3aa2947:C=1607bf8fb38:P=1cd:A=en-devtoken:V=2:H=340ffdbe6e147a1c053eb28f877ac446"
+
+# Connect client
+client = EvernoteClient(token=oauth_token)
+
+# Get note store
+note_store = client.get_note_store()
+
+# Create note
+# Upload note
